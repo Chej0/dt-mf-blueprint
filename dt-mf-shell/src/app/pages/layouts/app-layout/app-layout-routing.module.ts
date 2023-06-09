fd2e7b4b010b9be436c6ba1be3f2521015977d03
Sergio Mendez <sergiomendez0705@gmail.com>
2023-06-08 21:59:09 -0600

    adding react-app

--- dt-mf-shell/src/app/pages/layouts/app-layout/app-layout-routing.module.ts
+++ dt-mf-shell/src/app/pages/layouts/app-layout/app-layout-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppLayoutComponent } from './app-layout.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
-
+import { startsWith, WebComponentWrapper, WebComponentWrapperOptions } from '@angular-architects/module-federation-tools';
 const routes: Routes = [
   { 
     path: '', component: AppLayoutComponent,
@@ -10,6 +10,16 @@ const routes: Routes = [
       {
         path: '',
         loadChildren: () => loadRemoteModule({ type: 'module', remoteEntry: 'http://localhost:4201/remoteEntry.js', exposedModule: './AngularModule' }).then((m) => m.AngularModule)
+      },
+      {
+        path: 'react',
+        component: WebComponentWrapper,
+        data: {
+          remoteEntry: 'http://localhost:4204/remoteEntry.js',
+          remoteName: 'react',
+          exposedModule: './web-components',
+          elementName: 'react-element'
+        } as WebComponentWrapperOptions
       },
     ]
   }
